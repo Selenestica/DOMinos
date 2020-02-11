766136072a756a14f738b216e16e6436271dc465
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-02-11 13:32:56 -0600

    created user route

---
+++ server/routes/userRoute.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+router.use(express.urlencoded({extended: false}))
+
+
+//registers a user
+router.post('/register-user', async (req, res) => {
+
+    let email = req.body.email,
+    password = req.body.password
+
+    let persistedUser = await User.findOne({email})
+
+    if (persistedUser === null) {
+        bcrypt.hash(password, SALT_ROUNDS, async(error, hash) => {
+            if (error) {
+                console.log(error)
+                res.send('Error creating user ¯\_(ツ)_/¯')
+            }
+            else {
+                let user = new User({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    address: {
+                        Street: req.body.street,
+                        City: req.body.city,
+                        Region: req.body.state,
+                        PostalCode: req.body.zip
+                    },
+                    email: req.body.email, 
+                    phone: req.body.phone,
+                    password: hash,
+                    pastOrders: {
+                        item: {
+                            code: req.body.item_code,
+                            options: req.body.item_options,
+                            quantity: req.body.item_quantity
+                        },
+                        date_ordered: req.body.date_ordered,
+                        delivery_address: {
+                            Street: req.body.street,
+                            City: req.body.city,
+                            Region: req.body.state,
+                            PostalCode: req.body.zip
+                        }
+                    }
+                })
+                let registeredUser = await user.save()
+
+                await console.log(registeredUser)
+                if (registeredUser !== null) {
+                    console.log('User registered!')
+                    res.send('User registered!')
+                }
+                else {
+                    res.send('There is already a user who has registered with that information ¯\_(ツ)_/¯')
+                }
+            }
+        })
+    }
+    else {
+        res.send('There is already a user who has registered with that information ¯\_(ツ)_/¯')
+    }
+})
+
+//user login
+router.post('/login', (req, res) => {
+    
+})
+
+//view all registered users
+router.get('/view-registered-users', (req, res) => {
+    User.find({}).then(users => res.json(users))
+})
+
+//unregister (delete) a user
+router.post('/unregister-user/:userId', (req, res) => {
+    const userId = req.params.userId
+    User.findOneAndDelete(userId).then(() => res.send('User removed from database.'))
+})
+
+//update registered user's info
+router.put('/view-registered-users/update/:userId', (req, res) => {
+    const userId = req.params.userId    
+
+    User.findByIdAndUpdate(userId, {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        address: {
+            Street: req.body.street,
+            City: req.body.city,
+            Region: req.body.state,
+            PostalCode: req.body.zip
+        },
+        email: req.body.email, 
+        phone: req.body.phone,
+        password: hash 
+    }).then(res.send('Profile updated!')).catch(error => res.send(error))
+})
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -6,113 +6,23 @@ const util = require('util')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const PORT = 1200
-const User = require('./models/user')
 const app = express()
 global.bcrypt = require('bcrypt')
 global.SALT_ROUNDS = 10
 const jwt = require('jsonwebtoken')
 
+const userRouter = require('./routes/userRoute')
+app.use('/user', userRouter)
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false)
 dotenv.config()
 
 //connecting to mongodb
 mongoose.connect('mongodb://localhost:27017/users', {useNewUrlParser: true, useUnifiedTopology: true})
 
-//registers a user
-app.post('/register-user', async (req, res) => {
-
-    let //firstName = req.body.firstName,
-    //lastName = req.body.lastName,
-    email = req.body.email,
-    //phone = req.body.phone
-    password = req.body.password
-
-    let persistedUser = await User.findOne({email})
-
-    if (persistedUser === null) {
-        bcrypt.hash(password, SALT_ROUNDS, async(error, hash) => {
-            if (error) {
-                console.log(error)
-                res.send('Error creating user ¯\_(ツ)_/¯')
-            }
-            else {
-                let user = new User({
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    address: {
-                        Street: req.body.street,
-                        City: req.body.city,
-                        Region: req.body.state,
-                        PostalCode: req.body.zip
-                    },
-                    email: req.body.email, 
-                    phone: req.body.phone,
-                    password: hash,
-                    pastOrders: {
-                        item: {
-                            code: req.body.item_code,
-                            options: req.body.item_options,
-                            quantity: req.body.item_quantity
-                        },
-                        date_ordered: req.body.date_ordered,
-                        delivery_address: {
-                            Street: req.body.street,
-                            City: req.body.city,
-                            Region: req.body.state,
-                            PostalCode: req.body.zip
-                        }
-                    }
-                })
-                let registeredUser = await user.save()
-
-                await console.log(registeredUser)
-                if (registeredUser !== null) {
-                    console.log('User registered!')
-                    res.send('User registered!')
-                }
-                else {
-                    res.send('There is already a user who has registered with that information ¯\_(ツ)_/¯')
-                }
-            }
-        })
-    }
-    else {
-        res.send('There is already a user who has registered with that information ¯\_(ツ)_/¯')
-    }
-})
-
-//view all registered users
-app.get('/view-registered-users', (req, res) => {
-    User.find({}).then(users => res.json(users))
-})
-
-//unregister (delete) a user
-app.post('/unregister-user/:userId', (req, res) => {
-    const userId = req.params.userId
-    User.findOneAndDelete(userId).then(() => res.send('User removed from database.'))
-})
-
-//update registered user's info
-app.put('/view-registered-users/update/:userId', (req, res) => {
-    const userId = req.params.userId    
-
-    User.findByIdAndUpdate(userId, {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        address: {
-            Street: req.body.street,
-            City: req.body.city,
-            Region: req.body.state,
-            PostalCode: req.body.zip
-        },
-        email: req.body.email, 
-        phone: req.body.phone,
-        password: hash 
-    }).then(res.send('Profile updated!')).catch(error => res.send(error))
-})
 
 
 
@@ -120,7 +30,7 @@ app.put('/view-registered-users/update/:userId', (req, res) => {
 
 
 
-
+// ************************************************ DOMINOS API ************************************************ //
 // ************************************************ PREP WORK FUNCTIONS AND EXAMPLES ************************************************ //
 /*
 //finds nearby stores... important for finding the id of the store nearest you
