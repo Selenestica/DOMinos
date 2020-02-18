3a51b571364e7e06e4383581e2050996e918f43f
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-02-18 17:48:17 -0600

    user is starting to see what is on their profile

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function UserProfile() {
+
+    //const userEmail = props.userEmail
+    const [userInfo, setUserInfo] = useState([])
+
+    useEffect(() => {
+        const userEmail = localStorage.getItem('userEmail')
+        fetch(`http://localhost:1200/user/view-user-profile/${userEmail}`)
+        .then(response => response.json())
+        .then(json => {
+            
+            const userData = Object.keys(json).map((key) => {
+
+                return (<>
+                
+                    <li>
+                        {key}
+                    </li>
+
+                </>)
+
+            })
+            setUserInfo(userData)
+        })
+    }, [])
 
     return(<>
     
         <div className="container">
 
             <h1>Your Pizza Profile</h1>
+            {userInfo}
 
         </div>
     
